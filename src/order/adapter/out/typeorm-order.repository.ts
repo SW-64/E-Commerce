c639f135f6c87995c1b4b13250e98840aca996b7
SW-64 <ettjddns6@gmail.com>
2025-10-17 18:34:11 +0900

    FIX : 패키지 업그레이드 및 반환 타입 불일치 해결

--- src/order/adapter/out/typeorm-order.repository.ts
+++ src/order/adapter/out/typeorm-order.repository.ts
@@ -1,6 +1,9 @@
 import { Repository } from "typeorm";
 import { OrderEntity, OrderStatus } from "./order.entity";
-import { OrderRepositoryPort, OrderView } from "src/order/port/out/order.repository";
+import {
+  OrderRepositoryPort,
+  OrderView,
+} from "src/order/port/out/order.repository";
 import { Order } from "../../domain/order";
 
 export class TypeOrmOrderRepository implements OrderRepositoryPort {
@@ -26,9 +29,23 @@ export class TypeOrmOrderRepository implements OrderRepositoryPort {
     await this.repo.update(orderId, { status: OrderStatus[status] });
   }
 
-    async findById(id: number): Promise<OrderView | null> {
-    const e = await this.repo.findOne({ where: { orderId: id }, relations: ['user','items']});
+  async findById(id: number): Promise<OrderView | null> {
+    const e = await this.repo.findOne({
+      where: { orderId: id },
+      relations: ["user", "items", "items.product"],
+    });
     if (!e) return null;
-    return e
+    return {
+      orderId: e.orderId,
+      userId: e.user.userId,
+      status: e.status,
+      totalAmount: e.totalAmount,
+      paidAmount: e.paidAmount,
+      items: e.orderItems.map((item) => ({
+        productId: item.product.productId,
+        quantity: item.quantity,
+        unitPrice: item.unitPrice,
+      })),
+    };
   }
 }
